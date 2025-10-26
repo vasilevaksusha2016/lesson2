0a787ad2e160a2a82d149705e8b49f8147fe776e
vasilevaksusha2016 <vasilevaksusha2016@gmail.com>
2025-10-26 14:29:27 +0300

    ооп

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -3,7 +3,7 @@ from mailing import Mailing
 
 to_address = Address(123456, "Москва", "Пушкина", 1, 100)
 from_address = Address(654321, "Санкт-Петербург", "Пушкина", 2, 200)
-price = 690
+price = 590
 track_num = "TN923473875"
 
 mailing = Mailing(to_address, from_address, price, track_num)
